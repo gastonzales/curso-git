43e11d1396144e70090a82dd35417dfb84a5bc8e
gastonzales <125465281+gastonzales@users.noreply.github.com>
2023-03-20 20:03:17 -0300

    agrego hola.py

---
+++ hola.py
@@ -0,0 +1,32 @@
+a=5/2
+print (f"hola {a}")
+
+rica=int (input ("ingrese edad de la hembra: "))
+
+if rica>15:
+    print("con pelito")
+else: 
+    print("sin pelito")
+
+# for variable in lista:
+num=0
+for num in range(5):
+    num+=1
+    print(f"{num}")
+
+print("\n")
+num=0
+while num <5:
+    print(num)
+    num+=1
+
+def saluda():
+    print("hola ")
+    
+
+saluda()
+
+def valor():
+    return 5
+val=valor()
+print(val)
